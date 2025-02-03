48f931c7a2b520ea1ea4929b0b5fe3b35753effe
Emmanuel Herrera <emmanuelherrerab31@gmail.com>
2025-02-03 03:42:21 -0500

    le agregue la seccion de los quices

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const path = require('path'); // Agregar esta línea
+const path = require('path');
+const mysql = require('mysql2'); // Requiere el paquete mysql2
 
 const app = express();
 const port = 3000;
+
+// Configuración de la conexión a la base de datos MySQL
+const db = mysql.createConnection({
+  host: 'localhost',   // o la IP del servidor MySQL si no está en la misma máquina
+  user: 'root',        // Tu usuario de MySQL
+  password: 'HUEvos11',  // Tu contraseña de MySQL
+  database: 'db_foxsavvy'  // Nombre de la base de datos
+});
+
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+  } else {
+    console.log('Conexión exitosa a la base de datos MySQL');
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,7 +29,6 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar el servidor
-
 app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://192.168.0.15:${port}`);
 });
